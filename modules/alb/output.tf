0807be0f318aafc7543f980898be078b3fb68dbf
Edward Viaene <ward@in4it.io>
2020-01-04 15:47:31 +0100

    fix output formatting

--- modules/alb/output.tf
+++ modules/alb/output.tf
@@ -1,18 +1,18 @@
 output "alb_arn" {
-  value = "${aws_alb.alb.arn}"
+  value = aws_alb.alb.arn
 }
 
 output "dns_name" {
-  value = "${aws_alb.alb.dns_name}"
+  value = aws_alb.alb.dns_name
 }
 
 output "zone_id" {
-  value = "${aws_alb.alb.zone_id}"
+  value = aws_alb.alb.zone_id
 }
 
 output "http_listener_arn" {
-  value = "${aws_alb_listener.alb-http.arn}"
+  value = aws_alb_listener.alb-http.arn
 }
 output "https_listener_arn" {
-  value = "${aws_alb_listener.alb-https.arn}"
+  value = aws_alb_listener.alb-https.arn
 }
