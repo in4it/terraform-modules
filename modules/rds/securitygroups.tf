13b4399590dc96fe3c438da86017fc78c126e03e
Alessandro Marino <15277744+alessandromr@users.noreply.github.com>
2021-07-06 13:13:40 +0200

    Fix Security Group Port for RDS Mysql (#9)
    * Fix RDS Mysql Port on SG
    
    * fix function name

--- modules/rds/securitygroups.tf
+++ modules/rds/securitygroups.tf
@@ -1,5 +1,5 @@
 locals {
-  port = var.engine_family == "mysql" ? 3306 : 5432
+  port = substr(var.engine_family, 0, 5) == "mysql" ? 3306 : 5432
 }
 resource "aws_security_group" "rds" {
   name        = "${var.name}-rds"
