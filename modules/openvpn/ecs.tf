798fbfd455a144dce18244ecbc83977625a1f82b
Alessandro Marino <alessandromarinoac@gmail.com>
2021-08-30 16:54:26 +0200

    Remove ECS Module

--- modules/openvpn/ecs.tf
+++ modules/openvpn/ecs.tf
@@ -1,8 +1,61 @@
-module "ecs-cluster" {
-  source                = "git@github.com:in4it/terraform-modules.git//modules/fargate-cluster"
-  cluster_name          = "${var.project_name}-vpn-${var.env}"
-  log_group             = "${var.project_name}-vpn-${var.env}"
-  execution_role_policy = data.aws_iam_policy_document.ecs_execution_role.json
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "${var.project_name}-vpn-${var.env}"
+}
+
+resource "aws_cloudwatch_log_group" "cluster" {
+  name = "${var.project_name}-vpn-${var.env}"
+}
+
+resource "aws_iam_role" "ecs-task-execution-role" {
+  name = "ecs-task-execution-role-${var.project_name}-vpn-${var.env}"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ecs-tasks.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ecs-task-execution-role-policy" {
+  name = "ecs-task-execution-role-policy-${var.project_name}-vpn-${var.env}"
+  role = aws_iam_role.ecs-task-execution-role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ecs-task-execution-custom-role-policy" {
+  name   = "ecs-task-execution-custom-role-policy-${var.project_name}-vpn-${var.env}"
+  role   = aws_iam_role.ecs-task-execution-role.id
+  policy = data.aws_iam_policy_document.ecs_execution_role.json
 }
 
 data "aws_iam_policy_document" "ecs_execution_role" {
