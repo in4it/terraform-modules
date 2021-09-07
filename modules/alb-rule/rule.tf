4b07eec79a194f92054cc91cb0390f6abf8a222a
Alessandro Marino <alessandromarinoac@gmail.com>
2021-09-07 17:32:20 +0200

    remove typo

--- modules/alb-rule/rule.tf
+++ modules/alb-rule/rule.tf
@@ -4,10 +4,9 @@ resource "aws_lb_listener_rule" "alb_rule" {
 
   action {
     type             = var.action_type
-    target_group_arn = var.target_group_arn m
+    target_group_arn = var.target_group_arn
 
-
-    dynamic fixed_response {
+    dynamic "fixed_response" {
       for_each = var.action_type == "fixed-response" ? [1] : []
       content {
         content_type = var.fixed_response.content_type
@@ -15,7 +14,7 @@ resource "aws_lb_listener_rule" "alb_rule" {
         status_code  = var.fixed_response.status_code
       }
     }
-    dynamic redirect {
+    dynamic "redirect" {
       for_each = var.action_type == "redirect" ? [1] : []
       content {
         port        = var.redirect.port
@@ -30,13 +29,13 @@ resource "aws_lb_listener_rule" "alb_rule" {
 
   # legacy code
   condition {
-    dynamic host_header {
+    dynamic "host_header" {
       for_each = var.condition_field == "host-header" ? [1] : []
       content {
         values = var.condition_values
       }
     }
-    dynamic path_pattern {
+    dynamic "path_pattern" {
       for_each = var.condition_field == "path-pattern" ? [1] : []
       content {
         values = var.condition_values
@@ -44,22 +43,22 @@ resource "aws_lb_listener_rule" "alb_rule" {
     }
   }
   # more flexible approach
-  dynamic condition {
+  dynamic "condition" {
     for_each = var.conditions
     content {
-      dynamic host_header {
+      dynamic "host_header" {
         for_each = condition.value.field == "host-header" ? [1] : []
         content {
           values = condition.value.values
         }
       }
-      dynamic path_pattern {
+      dynamic "path_pattern" {
         for_each = condition.value.field == "path-pattern" ? [1] : []
         content {
           values = condition.value.values
         }
       }
-      dynamic query_string {
+      dynamic "query_string" {
         for_each = condition.value.field == "query-string" ? [1] : []
         content {
           value = condition.value.value
