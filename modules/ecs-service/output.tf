4ed3fd95079989de5f0d0a6ddc59cbab688366e1
Alessandro Marino <alessandromarinoac@gmail.com>
2021-09-07 18:00:19 +0200

    Fix output

--- modules/ecs-service/output.tf
+++ modules/ecs-service/output.tf
@@ -1,13 +1,13 @@
 output "target_group_arns" {
-  value = [for ecs-service in aws_lb_target_group.ecs-service : ecs-service.arn]
+  value = var.enable_alb ? [for ecs-service in aws_lb_target_group.ecs-service : ecs-service.arn] : []
 }
 
 output "target_group_arn" {
-  value = element([for ecs-service in aws_lb_target_group.ecs-service : ecs-service.arn], 0)
+  value = var.enable_alb ? element([for ecs-service in aws_lb_target_group.ecs-service : ecs-service.arn], 0) : ""
 }
 
 output "target_group_names" {
-  value = [for ecs-service in aws_lb_target_group.ecs-service : ecs-service.name]
+  value = var.enable_alb ? [for ecs-service in aws_lb_target_group.ecs-service : ecs-service.name] : []
 }
 
 output "task_security_group_id" {
