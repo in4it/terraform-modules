6ce5284291cdd7c1cd634781ccd3199c63f6a3ac
Alessandro Marino <alessandromarinoac@gmail.com>
2021-09-20 16:27:20 +0200

    Use .env files

--- modules/ecs-service/ecs-service.tf
+++ modules/ecs-service/ecs-service.tf
@@ -40,6 +40,7 @@ locals {
       mountpoints         = var.mountpoints
       secrets             = var.secrets
       environments        = var.environments
+      environmentFiles    = var.environment_files
     }]
   }
 }
