33a5b0d21acccfcb6e38f53ba316087f327b18b9
Alessandro Marino <alessandromarinoac@gmail.com>
2022-01-07 15:47:03 +0100

    Fix log parameter

--- modules/fargate-cluster/ecs.tf
+++ modules/fargate-cluster/ecs.tf
@@ -12,8 +12,7 @@ resource "aws_ecs_cluster" "cluster" {
         logging = "OVERRIDE"
 
         log_configuration {
-          cloud_watch_encryption_enabled = true
-          cloud_watch_log_group_name     = aws_cloudwatch_log_group.execute_command_logs[0].name
+          cloud_watch_log_group_name = aws_cloudwatch_log_group.execute_command_logs[0].name
         }
       }
     }
