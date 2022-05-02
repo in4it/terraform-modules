299bd756f9f1964dc17dbcc5df36253b36664eb5
Alessandro Marino <alessandromarinoac@gmail.com>
2022-05-02 15:45:49 +0200

    Add secrets + fixes

--- modules/ecs-service/vars.tf
+++ modules/ecs-service/vars.tf
@@ -207,9 +207,10 @@ variable "containers" {
       value = string
       type  = string
     }))
-    logconfiguration = object({
-      logdriver = string
-      options   = map(string)
+    logConfiguration = object({
+      logDriver     = string
+      options       = map(any)
+      secretOptions = list(map(string))
     })
   }))
 }
