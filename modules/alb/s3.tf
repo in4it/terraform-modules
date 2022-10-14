5ade9cf1b6fa4908d529805fe0d2f577f3609a87
Andre Narciso <andre.narciso@in4it.io>
2022-10-14 17:10:43 +0100

    enforce ssl policy + s3 refactor to v4

--- modules/alb/s3.tf
+++ modules/alb/s3.tf
@@ -11,22 +11,33 @@ resource "aws_s3_bucket" "lb_logs" {
   tags = {
     Name = "${var.lb_name}-lb-logs"
   }
+}
 
-  versioning {
-    enabled = false
-  }
+resource "aws_s3_bucket_server_side_encryption_configuration" "lb_logs" {
+  count  = length(var.access_logs)
+  bucket = aws_s3_bucket.lb_logs[0].id
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
+}
 
+resource "aws_s3_bucket_versioning" "lb_logs" {
+  count  = length(var.access_logs)
+  bucket = aws_s3_bucket.lb_logs[0].id
+  versioning_configuration {
+    status = "Disable"
+  }
+}
+
+resource "aws_s3_bucket_policy" "lb_logs" {
+  count  = length(var.access_logs)
+  bucket = aws_s3_bucket.lb_logs[0].id
   policy = <<EOF
 {
-    "Version": "2008-10-17",
+    "Version": "2012-10-17",
     "Statement": [
       {
         "Sid": "logs-policy",
@@ -40,12 +51,26 @@ resource "aws_s3_bucket" "lb_logs" {
         "Resource": [
           "arn:aws:s3:::${var.lb_name}-lb-logs/AWSLogs/*"
         ]
-      }
+      },
+      {
+        "Sid": "AllowSSLRequestsOnly",
+        "Action": "s3:*",
+        "Effect": "Deny",
+        "Resource": [
+          "arn:aws:s3:::${var.lb_name}-lb-logs",
+          "arn:aws:s3:::${var.lb_name}-lb-logs/*"
+        ],
+        "Condition": {
+          "Bool": {
+            "aws:SecureTransport": "false"
+          }
+        },
+        "Principal": "*"
+      }     
     ]
 }
 EOF
 }
-
 
 resource "aws_s3_bucket_public_access_block" "lb-logs" {
   count  = length(var.access_logs)
