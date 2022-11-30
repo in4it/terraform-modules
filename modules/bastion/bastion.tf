d695d03b2485b3a66b17a50b3924d37da39c9bbb
Andre Narciso <37899965+andrenarciso4@users.noreply.github.com>
2022-11-30 15:41:03 +0000

    update ubuntu + add ssm manager support (#37)

--- modules/bastion/bastion.tf
+++ modules/bastion/bastion.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
   filter {
--- modules/bastion/iam.tf
+++ modules/bastion/iam.tf
@@ -19,3 +19,7 @@ resource "aws_iam_role" "bastion" {
   assume_role_policy = data.aws_iam_policy_document.bastion_role_assume_role_policy.json
 }
 
+resource "aws_iam_role_policy_attachment" "bastion" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
