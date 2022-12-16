88fcb4412438e6acf3c94c71d13f8cbd0926cac0
Alessandro Marino <alessandromarinoac@gmail.com>
2022-12-16 19:21:59 +0100

    Update security hub

--- modules/cis/cis-general-organization/security-hub.tf
+++ modules/cis/cis-general-organization/security-hub.tf
@@ -1,7 +1,4 @@
+resource "aws_securityhub_account" "security_hub" {}
 resource "aws_securityhub_organization_admin_account" "security_hub" {
   admin_account_id = var.aws_account_id
-}
-
-resource "aws_securityhub_organization_configuration" "security_hub" {
-  auto_enable = true
 }
---
+++ modules/cis/cis-general/security-hub.tf
@@ -0,0 +1,3 @@
+resource "aws_securityhub_organization_configuration" "security_hub" {
+  auto_enable = true
+}
