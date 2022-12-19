cdf756a6a22325bd005c72065ec107d32d46e14a
Alessandro Marino <alessandromarinoac@gmail.com>
2022-12-19 18:47:17 +0100

    Remove unused security hub accepter

--- modules/cis/cis-general/security-hub.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_securityhub_account" "security_hub" {}
-
-resource "aws_securityhub_invite_accepter" "security_hub" {
-  count = var.aws_account_id == var.security_hub_master_id ? 0 : 1
-
-  depends_on = [aws_securityhub_account.security_hub]
-  master_id  = var.security_hub_master_id
-}
