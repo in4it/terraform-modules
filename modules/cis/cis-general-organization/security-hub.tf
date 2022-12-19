9ee9ab78474a97d0891a734f5eae359bc2284362
Alessandro Marino <alessandromarinoac@gmail.com>
2022-12-19 18:33:51 +0100

    Fix already existing securityhub account

--- modules/cis/cis-general-organization/security-hub.tf
+++ modules/cis/cis-general-organization/security-hub.tf
@@ -1,13 +1,8 @@
-resource "aws_securityhub_account" "security_hub" {}
 resource "aws_securityhub_organization_admin_account" "security_hub" {
-  depends_on = [aws_securityhub_account.security_hub]
-
   admin_account_id = var.aws_account_id
 }
-
 resource "aws_securityhub_member" "security_hub" {
   for_each   = var.security_hub_members
-  depends_on = [aws_securityhub_account.security_hub]
   account_id = each.value
   email      = each.key
 }
