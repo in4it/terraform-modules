d66cc0d6cd9ce5d6fdfd221a062bc6f02338f9f5
Alessandro Marino <15277744+alessandromr@users.noreply.github.com>
2023-01-20 18:21:59 +0100

    Fix missing templates on vpn (#39)
    remediate template code

--- modules/openvpn/configuration-files.tf
+++ modules/openvpn/configuration-files.tf
@@ -1,17 +1,21 @@
 resource "aws_s3_bucket_object" "oneloginconf" {
-  bucket  = aws_s3_bucket.configuration-bucket.id
-  key     = "openvpnconfig/onelogin.conf"
+  bucket = aws_s3_bucket.configuration-bucket.id
+  key    = "openvpnconfig/onelogin.conf"
   content = templatefile("${path.module}/tpl/onelogin-conf.tpl", {
     subdomain     = var.onelogin_client_domain
     client_id     = var.onelogin_client_id
     client_secret = var.onelogin_client_secret
   })
-  etag    = md5(data.template_file.oneloginconf.rendered)
+  etag = md5(templatefile("${path.module}/tpl/onelogin-conf.tpl", {
+    subdomain     = var.onelogin_client_domain
+    client_id     = var.onelogin_client_id
+    client_secret = var.onelogin_client_secret
+  }))
 }
 
 resource "aws_s3_bucket_object" "openvpn-vars" {
-  bucket  = aws_s3_bucket.configuration-bucket.id
-  key     = "openvpnconfig/vars"
+  bucket = aws_s3_bucket.configuration-bucket.id
+  key    = "openvpnconfig/vars"
   content = templatefile("${path.module}/tpl/openvpn-vars.tpl", {
     domain       = var.vpn_domain
     req_email    = var.cert_req_email
@@ -20,7 +24,14 @@ resource "aws_s3_bucket_object" "openvpn-vars" {
     req_country  = var.cert_req_country
     req_org      = var.certificate_organization_name
   })
-  etag    = md5(data.template_file.openvpn-vars.rendered)
+  etag = md5(templatefile("${path.module}/tpl/openvpn-vars.tpl", {
+    domain       = var.vpn_domain
+    req_email    = var.cert_req_email
+    req_city     = var.cert_req_city
+    req_province = var.cert_req_province
+    req_country  = var.cert_req_country
+    req_org      = var.certificate_organization_name
+  }))
 }
 
 resource "aws_s3_bucket_object" "openvpn-client" {
