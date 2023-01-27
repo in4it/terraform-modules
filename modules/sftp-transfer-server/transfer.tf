90122832d7d49c72505ed9069d838dc0ab9d0307
Alessandro Marino <alessandromarinoac@gmail.com>
2023-01-27 12:37:15 +0100

    Add homedir to options

--- modules/sftp-transfer-server/transfer.tf
+++ modules/sftp-transfer-server/transfer.tf
@@ -3,26 +3,26 @@ resource "aws_transfer_server" "transfer_server" {
   logging_role           = aws_iam_role.transfer_server_role.arn
 
   tags = merge(
-  {
-    NAME = var.transfer_server_name
-  },
-  var.tags
+    {
+      NAME = var.transfer_server_name
+    },
+    var.tags
   )
 }
 
 resource "aws_transfer_user" "transfer_server_user" {
-  count = length(var.transfer_server_user_names)
+  for_each = var.transfer_server_users
 
   server_id      = aws_transfer_server.transfer_server.id
-  user_name      = element(var.transfer_server_user_names, count.index)
+  user_name      = each.value.name
   role           = aws_iam_role.transfer_server_role.arn
-  home_directory = "/${var.bucket_name}"
+  home_directory = "/${each.value.homedir}"
 }
 
 resource "aws_transfer_ssh_key" "transfer_server_ssh_key" {
-  count = length(var.transfer_server_user_names)
+  for_each = var.transfer_server_users
 
   server_id = aws_transfer_server.transfer_server.id
-  user_name = element(aws_transfer_user.transfer_server_user.*.user_name, count.index)
-  body      = element(var.transfer_server_ssh_keys, count.index)
+  user_name = each.value.name
+  body      = each.value.key
 }
--- modules/sftp-transfer-server/vars.tf
+++ modules/sftp-transfer-server/vars.tf
@@ -16,12 +16,11 @@ variable "transfer_server_name" {
   type        = string
 }
 
-variable "transfer_server_user_names" {
-  description = "User name(s) for SFTP server"
-  type        = list(string)
-}
-
-variable "transfer_server_ssh_keys" {
-  description = "SSH Key(s) for transfer server user(s)"
-  type        = list(string)
+variable "transfer_server_users" {
+  description = "Transfer server user(s)"
+  type = list(object({
+    name    = string
+    key     = string
+    homedir = string
+  }))
 }
