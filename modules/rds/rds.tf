faaa656a9ef98f481a799f02500e819aa99b2860
Alessandro Marino <alessandromarinoac@gmail.com>
2023-05-15 18:26:31 +0200

    Remove default paramater

--- modules/rds/rds.tf
+++ modules/rds/rds.tf
@@ -38,7 +38,6 @@ resource "aws_db_instance" "rds" {
 data "aws_rds_engine_version" "rds_version" {
   engine             = var.engine
   preferred_versions = [var.engine_version]
-  default_only       = true
 }
 
 resource "aws_db_subnet_group" "rds" {
