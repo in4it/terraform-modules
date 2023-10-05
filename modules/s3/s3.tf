9b312f5f9bc0aa43171d8d2c6540406cda66010b
Vasilis Siourdas <53100368+siourdas-vasilis@users.noreply.github.com>
2023-10-05 20:47:51 +0300

    S3/expand policy (#76)
    * Add additional policy statements as a variable
    
    * add default value for new variable
    
    * fix dynamic block
    
    * Add public-access block var
    
    * Add cloudfront_allow_path access
    
    ---------
    
    Co-authored-by: Vasilis Siourdas <vasilis.siourdas@in4it.io>

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -9,10 +9,10 @@ resource "aws_s3_bucket" "this" {
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = var.public_access_block.block_public_acls
+  block_public_policy     = var.public_access_block.block_public_policy
+  ignore_public_acls      = var.public_access_block.ignore_public_acls
+  restrict_public_buckets = var.public_access_block.restrict_public_buckets
 }
 
 resource "aws_s3_bucket_versioning" "this" {
@@ -53,10 +53,10 @@ data "aws_iam_policy_document" "this" {
       }
       effect  = "Allow"
       actions = var.cloudfront_origin_access_identity_iam_actions
-      resources = [
+      resources = var.cloudfront_allow_path == "" ? [
         "arn:aws:s3:::${aws_s3_bucket.this.id}",
         "arn:aws:s3:::${aws_s3_bucket.this.id}/*",
-      ]
+      ] : ["${aws_s3_bucket.this.arn}/${var.cloudfront_allow_path}"]
     }
   }
   statement {
@@ -77,6 +77,27 @@ data "aws_iam_policy_document" "this" {
       test     = "Bool"
       values   = ["false"]
       variable = "aws:secureTransport"
+    }
+  }
+  dynamic "statement" {
+    for_each = var.additional_policy_statements
+    content {
+      sid = try(statement.value.sid, "AdditionS3Policy${statement.key}")
+      principals {
+        identifiers = statement.value.principals.identifiers
+        type        = statement.value.principals.type
+      }
+      effect    = statement.value.effect
+      actions   = statement.value.actions
+      resources = statement.value.resources
+      dynamic "condition" {
+        for_each = statement.value.condition == null ? [] : [statement.value.condition]
+        content {
+          test     = condition.value.test
+          values   = condition.value.values
+          variable = condition.value.variable
+        }
+      }
     }
   }
 }
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -17,3 +17,42 @@ variable "cloudfront_origin_access_identity_iam_actions" {
   type        = list(string)
   default     = ["s3:Get*"]
 }
+variable "additional_policy_statements" {
+  description = "additional policy statements to add to the s3 bucket policy"
+  type = list(object({
+    sid = optional(string)
+    principals = object({
+      identifiers = list(string)
+      type        = string
+    })
+    effect    = string
+    actions   = list(string)
+    resources = list(string)
+    condition = optional(object({
+      test     = string
+      values   = list(string)
+      variable = string
+    }))
+  }))
+  default = []
+}
+variable "public_access_block" {
+  description = "values for the public access block"
+  type = object({
+    block_public_acls       = bool
+    block_public_policy     = bool
+    ignore_public_acls      = bool
+    restrict_public_buckets = bool
+  })
+  default = {
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+  }
+}
+variable "cloudfront_allow_path" {
+  description = "S3 path to allow cloudfront access to. Default allows access to the entire bucket."
+  type        = string
+  default     = ""
+}
