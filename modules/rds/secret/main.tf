918fb0afbe05f9c4794b285ade1f9743eee55eb4
Dmytro Khaynas <dmytro@khainas.dev>
2023-11-06 20:45:30 +0000

    Create random secret module (#86)

---
+++ modules/rds/secret/main.tf
@@ -0,0 +1,23 @@
+resource "random_password" "password" {
+  length           = var.password_length
+  special          = true
+  override_special = var.password_override_special
+}
+
+resource "aws_secretsmanager_secret" "secret" {
+  name = var.name
+  description = var.description
+}
+
+resource "aws_secretsmanager_secret_version" "secret_value" {
+  secret_id     = aws_secretsmanager_secret.secret.id
+  secret_string = jsonencode({
+    username = var.username
+    password = random_password.password.result
+    engine   = var.engine
+    host     = var.host
+    port     = var.port
+    dbname   = var.dbname
+    dbInstanceIdentifier = var.dbInstanceIdentifier
+  })
+}
