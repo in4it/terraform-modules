15cd934f36e03a2a68e04533f02515304150068e
dmytro.khainas <dmytro.khainas@in4it.io>
2023-11-07 12:04:02 +0000

    Output secret name

--- modules/rds/secret/output.tf
+++ modules/rds/secret/output.tf
@@ -2,6 +2,10 @@ output "secret_id" {
   value = aws_secretsmanager_secret.secret.id
 }
 
+output "secret_name" {
+  value = aws_secretsmanager_secret.secret.name
+}
+
 output "secret_arn" {
   value = aws_secretsmanager_secret.secret.arn
 }
