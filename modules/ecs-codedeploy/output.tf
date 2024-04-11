400794b66a122a580ce70d67151ffb6f3fd79142
dmytro.khainas <dmytro.khainas@in4it.io>
2024-04-11 16:40:39 +0100

    Allow to attach policies to CodeDeploy

--- modules/ecs-codedeploy/output.tf
+++ modules/ecs-codedeploy/output.tf
@@ -6,6 +6,10 @@ output "application_arn" {
   value = aws_codedeploy_app.codedeploy.arn
 }
 
+output "deploy_role_id" {
+  value = aws_iam_role.codedeploy.id
+}
+
 output "deployment_group_name" {
   value = aws_codedeploy_deployment_group.codedeploy.deployment_group_name
 }
