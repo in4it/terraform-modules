f50e1740bcb60e9911b44b13735cdfb822c81ef4
dmytro.khainas <dmytro.khainas@in4it.io>
2024-04-25 16:39:42 +0100

    Refactor vpn to new ecs module

--- modules/openvpn/ecs-openvpn-access.tf
+++ modules/openvpn/ecs-openvpn-access.tf
@@ -1,4 +1,3 @@
-
 module "openvpn-access" {
   source                    = "git@github.com:in4it/terraform-modules.git//modules/ecs-service"
   vpc_id                    = var.vpc_id
@@ -10,7 +9,6 @@ module "openvpn-access" {
   healthcheck_path          = "/"
   cpu_reservation           = "256"
   memory_reservation        = "512"
-  log_group                 = aws_cloudwatch_log_group.cloudwatch-ecs-openvpn-access.name
   desired_count             = 1
   alb_arn                   = var.alb_arn
   launch_type               = "FARGATE"
@@ -29,64 +27,24 @@ module "openvpn-access" {
       application_name    = "openvpn-access"
       application_port    = "8080"
       host_port           = null
-      additional_ports    = []
       application_version = "latest"
       cpu_reservation     = "256"
       memory_reservation  = "512"
-      links               = []
-      dependsOn           = []
-      mountpoints         = []
-      command             = []
-      environments = [
-        {
-          name  = "STORAGE_TYPE"
-          value = "S3"
-        },
-        {
-          name  = "S3_BUCKET"
-          value = aws_s3_bucket.configuration-bucket.id
-        },
-        {
-          name  = "S3_PREFIX"
-          value = "openvpn"
-        },
-        {
-          name  = "AWS_REGION"
-          value = data.aws_region.current.name
-        },
-      ]
-      secrets = [
-        {
-          name      = "OAUTH2_CLIENT_ID"
-          valueFrom = var.ouath2_client_id_parameter_arn
-        },
-        {
-          name      = "CSRF_KEY"
-          valueFrom = var.csrf_key_parameter_arn
-        }
-        , {
-          name      = "CLIENT_CERT_ORG"
-          valueFrom = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.project_name}-${var.env}/vpn/CLIENT_CERT_ORG"
-        },
-        {
-          name      = "OAUTH2_CLIENT_SECRET"
-          valueFrom = var.ouath2_client_secret_parameter_arn
-        },
-        {
-          name      = "OAUTH2_REDIRECT_URL"
-          valueFrom = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.project_name}-${var.env}/vpn/OAUTH2_REDIRECT_URL"
-        },
-        {
-          name      = "OAUTH2_SCOPES"
-          valueFrom = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.project_name}-${var.env}/vpn/OAUTH2_SCOPES"
-        },
-        {
-          name      = "OAUTH2_URL"
-          valueFrom = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.project_name}-${var.env}/vpn/OAUTH2_URL"
-        },
-      ]
-      environment_files = []
-      docker_labels     = {}
+      environments = {
+        STORAGE_TYPE = "S3"
+        S3_BUCKET    = aws_s3_bucket.configuration-bucket.id
+        S3_PREFIX    = "openvpn"
+        AWS_REGION   = data.aws_region.current.name
+      }
+      secrets = {
+        OAUTH2_CLIENT_ID     = var.ouath2_client_id_parameter_arn
+        CSRF_KEY             = var.csrf_key_parameter_arn
+        CLIENT_CERT_ORG      = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.project_name}-${var.env}/vpn/CLIENT_CERT_ORG"
+        OAUTH2_CLIENT_SECRET = var.ouath2_client_secret_parameter_arn
+        OAUTH2_REDIRECT_URL  = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.project_name}-${var.env}/vpn/OAUTH2_REDIRECT_URL"
+        OAUTH2_SCOPES        = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.project_name}-${var.env}/vpn/OAUTH2_SCOPES"
+        OAUTH2_URL           = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.project_name}-${var.env}/vpn/OAUTH2_URL"
+      }
     }
   ]
 
@@ -99,9 +57,4 @@ module "openvpn-access" {
       security_groups = [var.alb_security_group_id]
     }
   ]
-}
-
-resource "aws_cloudwatch_log_group" "cloudwatch-ecs-openvpn-access" {
-  name              = "ecs-${var.project_name}-openvpn-access-${var.env}"
-  retention_in_days = var.log_retention_days
 }
