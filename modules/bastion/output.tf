9ca9edd0ca917d0eb407c9e6a16346a7a1090f67
dmytro.khainas <dmytro.khainas@in4it.io>
2024-06-17 18:48:12 +0100

    allow attach role policies

--- modules/bastion/output.tf
+++ modules/bastion/output.tf
@@ -2,6 +2,10 @@ output "security-group-id" {
   value = aws_security_group.bastion.id
 }
 
+output "bastion-iam-role" {
+  value = aws_iam_role.bastion.id
+}
+
 output "instance-id" {
   value = aws_instance.bastion.id
 }
