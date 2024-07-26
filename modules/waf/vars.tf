faf4d84d309f624a93eea7399e5d1c5758ce12f6
Vasilis Siourdas <53100368+siourdas-vasilis@users.noreply.github.com>
2024-07-26 15:20:32 +0300

    WAF/regex rules (#109)
    * waf: add regex rules
    * waf: complete integration with regex_match_statement

--- modules/waf/vars.tf
+++ modules/waf/vars.tf
@@ -2,9 +2,18 @@ variable "name" {
   description = "name of WAF"
   default     = "alb-waf"
 }
+variable "scope" {
+  description = "scope of WAF, use 'CLOUDFRONT' for CloudFront distributions"
+  default     = "REGIONAL"
+  validation {
+    condition     = var.scope == "REGIONAL" || var.scope == "CLOUDFRONT"
+    error_message = "scope must be REGIONAL or CLOUDFRONT"
+  }
+}
 variable "lb_arns" {
-  type = set(string)
-  description = "ARN of ALBs"
+  type        = set(string)
+  description = "ARN of ALBs to associate with WAF"
+  default     = []
 }
 
 variable "env" {
@@ -34,6 +43,16 @@ variable "managed_rules" {
     blocking_rules           = optional(list(string))
     allowing_rules           = optional(list(string))
     counting_rules           = optional(list(string))
+  }))
+  default = []
+}
+variable "regex_match_rules" {
+  type = list(object({
+    name       = string
+    priority   = number
+    action     = string # "count" or "block"
+    statement  = any
+    rule_label = optional(list(string), null)
   }))
   default = []
 }
--- modules/waf/waf.tf
+++ modules/waf/waf.tf
@@ -1,6 +1,6 @@
 resource "aws_wafv2_web_acl" "this" {
   name  = var.env == "" ? var.name : "${var.name}-${var.env}"
-  scope = "REGIONAL"
+  scope = var.scope
 
   default_action {
     allow {}
@@ -73,9 +73,9 @@ resource "aws_wafv2_web_acl" "this" {
           vendor_name = rule.value.managed_rule_vendor_name
           dynamic "rule_action_override" {
             for_each = merge(
-              { for r in coalesce(rule.value.blocking_rules,[]) : r => "block" },
-              { for r in coalesce(rule.value.allowing_rules,[]) : r => "allow" },
-              { for r in coalesce(rule.value.counting_rules,[]) : r => "count"}
+              { for r in coalesce(rule.value.blocking_rules, []) : r => "block" },
+              { for r in coalesce(rule.value.allowing_rules, []) : r => "allow" },
+              { for r in coalesce(rule.value.counting_rules, []) : r => "count" }
             )
             content {
               name = rule_action_override.key
@@ -103,7 +103,92 @@ resource "aws_wafv2_web_acl" "this" {
 
       visibility_config {
         cloudwatch_metrics_enabled = true
-        metric_name                = "ratelimit-${rule.value.name}"
+        metric_name                = "managedrules-${rule.value.name}"
+        sampled_requests_enabled   = true
+      }
+    }
+  }
+  dynamic "rule" {
+    for_each = { for r in var.regex_match_rules : r.name => r }
+    content {
+      name     = rule.value.name
+      priority = rule.value.priority
+      action {
+        dynamic "block" {
+          for_each = rule.value.action == "block" ? [1] : []
+          content {
+          }
+        }
+        dynamic "count" {
+          for_each = rule.value.action == "count" ? [1] : []
+          content {
+          }
+        }
+        dynamic "allow" {
+          for_each = rule.value.action == "allow" ? [1] : []
+          content {
+          }
+        }
+      }
+      statement {
+        dynamic "regex_match_statement" {
+          for_each = lookup(rule.value, "statement", null) != null ? [rule.value.statement] : []
+
+          content {
+            regex_string = regex_match_statement.value.regex_string
+
+            dynamic "field_to_match" {
+              for_each = lookup(rule.value.statement, "field_to_match", null) != null ? [rule.value.statement.field_to_match] : []
+
+              content {
+                dynamic "all_query_arguments" {
+                  for_each = lookup(field_to_match.value, "all_query_arguments", null) != null ? [1] : []
+                  content {}
+                }
+                dynamic "body" {
+                  for_each = lookup(field_to_match.value, "body", null) != null ? [1] : []
+                  content {}
+                }
+                dynamic "method" {
+                  for_each = lookup(field_to_match.value, "method", null) != null ? [1] : []
+
+                  content {}
+                }
+                dynamic "query_string" {
+                  for_each = lookup(field_to_match.value, "query_string", null) != null ? [1] : []
+
+                  content {}
+                }
+                dynamic "single_header" {
+                  for_each = lookup(field_to_match.value, "single_header", null) != null ? [field_to_match.value.single_header] : []
+
+                  content {
+                    name = single_header.value.name
+                  }
+                }
+                dynamic "single_query_argument" {
+                  for_each = lookup(field_to_match.value, "single_query_argument", null) != null ? [field_to_match.value.single_query_argument] : []
+                  content {
+                    name = single_query_argument.value.name
+                  }
+                }
+                dynamic "uri_path" {
+                  for_each = lookup(field_to_match.value, "uri_path", null) != null ? [1] : []
+                  content {}
+                }
+              }
+            }
+            text_transformation {
+              priority = 0
+              type     = "NONE"
+            }
+          }
+        }
+      }
+
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = "regex-${rule.value.name}"
         sampled_requests_enabled   = true
       }
     }
