435b82a88c6de467df7a6686a89732017fead235
Edward Viaene <ward@in4it.io>
2024-08-26 08:58:55 -0500

    more mount points for vpn-server

--- modules/wireguard-vpn-server/templates/userdata.sh
+++ modules/wireguard-vpn-server/templates/userdata.sh
@@ -10,6 +10,7 @@ systemctl stop vpn-configmanager
 wg-quick down vpn
 rm -rf /vpn/config
 rm -rf /vpn/secrets
+rm -rf /vpn/tls-certs
 
 # efs helper
 cd /root
@@ -29,6 +30,8 @@ rm /amazon-efs-utils*deb
 echo -e "${efs_fs_id}\t/efs\tefs\t_netdev,noresvport,tls" >> /etc/fstab
 echo -e "${efs_fs_id}:/config\t/vpn/config\tefs\t_netdev,noresvport,tls" >> /etc/fstab
 echo -e "${efs_fs_id}:/secrets\t/vpn/secrets\tefs\t_netdev,noresvport,tls" >> /etc/fstab
+echo -e "${efs_fs_id}:/tls-certs\t/vpn/tls-certs\tefs\t_netdev,noresvport,tls" >> /etc/fstab
+echo -e "${efs_fs_id}:/stats\t/vpn/stats\tefs\t_netdev,noresvport,tls" >> /etc/fstab
 
 # require mounts before starting the vpn server
 sed -i 's#\[Unit\]#[Unit]\nRequires=vpn-config.mount vpn-secrets.mount\nAfter=vpn-config.mount vpn-secrets.mount#' /etc/systemd/system/vpn-configmanager.service
@@ -41,16 +44,26 @@ systemctl daemon-reload
 mkdir -p /efs
 mount /efs
 mkdir -p /efs/config
+mkdir -p /efs/secrets
+mkdir -p /efs/tls-certs
+mkdir -p /efs/stats
 chown vpn:vpn /efs/config
+chown vpn:vpn /efs/tls-certs
+chown vpn:vpn /efs/stats
 chmod 700 /efs
 chmod 700 /efs/config
-mkdir -p /efs/secrets
+chmod 700 /efs/tls-certs
 chmod 700 /efs/secrets
+chmod 700 /efs/stats
 
 mkdir /vpn/config
 mount /vpn/config
 mkdir /vpn/secrets
 mount /vpn/secrets
+mkdir /vpn/tls-certs
+mount /vpn/tls-certs
+mkdir /vpn/stats
+mount /vpn/stats
 
 # restart vpn
 systemctl start vpn-rest-server
