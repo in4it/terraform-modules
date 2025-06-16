c4ee8e10192be7e60b75fe52ca5ad243ac254d40
Vasilis Siourdas <vasilis.siourdas@in4it.io>
2025-06-16 16:42:59 +0200

    Move changes in the current module.

--- modules/elasticache-redis/output.tf
+++ modules/elasticache-redis/output.tf
@@ -1,10 +1,10 @@
 output "endpoint" {
-  value = var.cluster_mode_enabled == false ? aws_elasticache_replication_group.redis.primary_endpoint_address : aws_elasticache_replication_group.redis.configuration_endpoint_address
+  value = var.cluster_mode_enabled == false ? aws_elasticache_replication_group.this.primary_endpoint_address : aws_elasticache_replication_group.this.configuration_endpoint_address
 }
 output "redis_security_group_id" {
-  value       = var.existing_security_group == "" ? aws_security_group.redis[0].id : var.existing_security_group
+  value       = var.existing_security_group == "" ? aws_security_group.this[0].id : var.existing_security_group
   description = "Deprecated: Use `security_group_id` instead"
 }
 output "security_group_id" {
-  value = var.existing_security_group == "" ? aws_security_group.redis[0].id : var.existing_security_group
+  value = var.existing_security_group == "" ? aws_security_group.this[0].id : var.existing_security_group
 }
--- modules/elasticache/output.tf
+++
@@ -1,6 +0,0 @@
-output "endpoint" {
-  value = var.cluster_mode_enabled == false ? aws_elasticache_replication_group.this.primary_endpoint_address : aws_elasticache_replication_group.this.configuration_endpoint_address
-}
-output "security_group_id" {
-  value = var.existing_security_group == "" ? aws_security_group.this[0].id : var.existing_security_group
-}
